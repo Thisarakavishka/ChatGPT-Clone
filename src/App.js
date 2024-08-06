905b8f9db2defd9933538f82a4611f52dc278266
thisarakavishka <thisarakavishka0@gmail.com>
2024-08-06 22:13:56 +0530

    Finish frontend

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import msgIcon from './assets/message.svg'
 import home from './assets/home.svg'
 import saved from './assets/bookmark.svg'
 import rocket from './assets/rocket.svg'
+import sendBtn from './assets/send.svg'
+import gptLogo from './assets/chatgptLogo.svg'
+import userIcon from './assets/user-icon.png'
+
 
 function App() {
   return (
@@ -16,40 +20,60 @@ function App() {
             <span className='brand'>ChatGPT</span>
           </div>
           <button className='midBtn'>
-            <img src={addBtn} className='addBtn'/>
+            <img src={addBtn} className='addBtn' />
             New Chat
           </button>
           <div className='upperSideBottom'>
             <button className='query'>
-              <img src={msgIcon}/>
+              <img src={msgIcon} />
               what is programming
             </button>
             <button className='query'>
-              <img src={msgIcon}/>
+              <img src={msgIcon} />
               How to use api
             </button>
             <button className='query'>
-              <img src={msgIcon}/>
+              <img src={msgIcon} />
               what is java
             </button>
           </div>
         </div>
         <div className='lowerSide'>
           <div className='listItems'>
-            <img src={home} alt='Home' className='listItemImg'/>
+            <img src={home} alt='Home' className='listItemImg' />
             Home
           </div>
           <div className='listItems'>
-            <img src={saved} alt='Saved' className='listItemImg'/>
+            <img src={saved} alt='Saved' className='listItemImg' />
             Saved
           </div>
           <div className='listItems'>
-            <img src={rocket} alt='Upgrade to pro' className='listItemImg'/>
+            <img src={rocket} alt='Upgrade to pro' className='listItemImg' />
             Upgrade to pro
           </div>
         </div>
       </div>
-      <div className='main'></div>
+      <div className='main'>
+        <div className='chats'>
+          <div className='chat'>
+            <img className='chat-img' src={userIcon} />
+            <p className='text'>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32</p>
+          </div>
+          <div className='chat bot'>
+            <img className='chat-img' src={gptLogo} />
+            <p className='text'>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32</p>
+          </div>
+        </div>
+        <div className='chatFooter'>
+          <div className='inp'>
+            <input type='text' placeholder='Send a message...' />
+            <button className='send'>
+              <img src={sendBtn} alt='send button' />
+            </button>
+          </div>
+          <p>ChatGPT can make mistakes. Check important info.</p>
+        </div>
+      </div>
     </div>
   );
 }
